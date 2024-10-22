5c04fb26061228223c680a4981739946560cc97c
MolackTime <34474303+MolackTime@users.noreply.github.com>
2024-10-22 20:21:03 +0800

    Update Live2DTest.Target.cs
    ue5.3

--- Source/Live2DTest.Target.cs
+++ Source/Live2DTest.Target.cs
@@ -8,8 +8,8 @@ public class Live2DTestTarget : TargetRules
 	public Live2DTestTarget( TargetInfo Target) : base(Target)
 	{
         Type = TargetType.Game;
-        DefaultBuildSettings = BuildSettingsVersion.V5;
-        IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
+        DefaultBuildSettings = BuildSettingsVersion.V4;
+        IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_3;
 
         ExtraModuleNames.AddRange( new string[] { "Live2DTest" } );
 	}
